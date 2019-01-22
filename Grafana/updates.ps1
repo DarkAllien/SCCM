b48939ee4a5fe48929c916e9162ad794e5dbaf2a
Octavian Cordos <cordos.octavian@gmail.com>
2019-01-22 13:22:55 +0200

    Update updates.ps1

--- Grafana/updates.ps1
+++ Grafana/updates.ps1
@@ -5,77 +5,6 @@ $Database ='<your SCCM Database>'
 $coll_stats = Import-Csv $dirfiles'\coll_stats.csv'
 
 #Functions
-Function Server_Versions{
-    [CmdletBinding()]
-        Param (
-            [Parameter(Mandatory = $true)]
-            [Alias('CollID')]
-            [String]$Coll_ID,
-            [Parameter(Mandatory = $true)]
-            [Alias('FilterName')]
-            [String]$Filter_Name
-            )
-    $sqlCmd = "
-    SELECT distinct
-    --[a].[operatingSystem0],
-     CASE
-               WHEN [a].[operatingSystem0] LIKE '%2008 R2%'
-               THEN '2008R2'
-               WHEN [a].[operatingSystem0] LIKE '%2008%'
-               THEN '2008'
-               WHEN [a].[operatingSystem0] LIKE '%2003%'
-               THEN '2003'
-               WHEN [a].[operatingSystem0] LIKE '%2012 R2%'
-               THEN '2012_R2'
-               WHEN [a].[operatingSystem0] LIKE '%2012%'
-               THEN '2012'
-               WHEN [a].[operatingSystem0] LIKE '%2016%'
-               THEN '2016'
-		       WHEN [a].[operatingSystem0] LIKE '%2019%'
-               THEN '2019'
-           END AS [OS],
-           --iif([C].[Value] is null,0,[C].[Value]) AS [Build],
-           COUNT(DISTINCT [A].[Name0]) AS [count]
-           FROM $Database.dbo.[v_R_System] [A]
-                LEFT OUTER JOIN $Database.dbo.[vSMS_WindowsServicingStates] [B] ON [B].[Build] = [A].[Build01]
-                                                                     AND (([B].[Branch] = [A].[OSBranch01])
-                                                                          OR ([A].[OSBranch01] = ''
-                                                                              AND [B].[Branch] = 0))
-                LEFT OUTER JOIN $Database.dbo.[vSMS_WindowsServicingLocalizedNames] [C] ON [B].[Name] = [C].[Name]
-                LEFT JOIN $Database.[dbo].[v_FullCollectionMembership] [vc] ON [vc].[ResourceID] = [A].[ResourceID]
-           WHERE [a].[operatingSystem0] IS NOT NULL
-                 AND [a].[operatingSystem0] LIKE '%server %'
-                 AND [vc].[CollectionID] = '$Coll_ID'
-    group by
-     CASE
-               WHEN [a].[operatingSystem0] LIKE '%2008 R2%'
-               THEN '2008R2'
-               WHEN [a].[operatingSystem0] LIKE '%2008%'
-               THEN '2008'
-               WHEN [a].[operatingSystem0] LIKE '%2003%'
-               THEN '2003'
-               WHEN [a].[operatingSystem0] LIKE '%2012 R2%'
-               THEN '2012_R2'
-               WHEN [a].[operatingSystem0] LIKE '%2012%'
-               THEN '2012'
-               WHEN [a].[operatingSystem0] LIKE '%2016%'
-               THEN '2016'
-		       WHEN [a].[operatingSystem0] LIKE '%2019%'
-               THEN '2019'
-           END--,[C].[Value]
- 
-    "
-    try {
-        $result = Invoke-Sqlcmd $sqlCmd -server $SQL_Server -Database $Database
-    }
-    catch {
-        ##return $false
-    }
-    foreach ($row in $result){
-        $body=$Filter_Name+'.Versions,Server=_'+$row.OS+' Count='+$row.count
-        write-host $body
-    }
-}
 Function SCEP{
     [CmdletBinding()]
         Param (
@@ -249,5 +178,4 @@ foreach ($Entry in $coll_stats) {
     if ($Entry.SCEP -eq 'True'){
         SCEP -CollID $Entry.CollID -CollType $Entry.CollType -FilterName $Entry.FilterName
     }
-    Server_versions -CollID $Entry.CollID -FilterName $Entry.FilterName
 }
